0d54ccc16f8e1f561c06d006ce693d8508433c2f
DeAndreFuentes <74945613+DeAndreFuentes@users.noreply.github.com>
2021-03-01 20:59:36 -0500

    comp_report

---
+++ intro_python/warehouse/intro_python/intro.py
@@ -0,0 +1,40 @@
+def separator():
+    print("________________")
+    print("\n")
+
+
+print("hello world")
+
+
+last_name = "Fuentes"
+name = "Deandre"
+found = False
+age = 41
+price = 34.55
+
+
+print(name + last_name)
+
+
+print(name + str(age))
+
+separator()
+
+if (age < 99):
+    print("Don't worry you are still here")
+    print("inside here")
+    if(4 > 2):  # nested if statment
+        print("It is greater")
+
+    if(age < 99):
+        print("ok")
+    elif(age == 99):
+        print("too bad")
+    else:
+        print("dog")
+
+
+separator()
+
+
+print("This is outside")
